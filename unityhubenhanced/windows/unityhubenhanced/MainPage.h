f169fa7b49f81179260282b00b6db056c3892a5d
Junaid Malik <junmalik109@gmail.com>
2023-03-16 09:46:27 +0000

    windows build folder with temp added

---
+++ unityhubenhanced/windows/unityhubenhanced/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::unityhubenhanced::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::unityhubenhanced::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
